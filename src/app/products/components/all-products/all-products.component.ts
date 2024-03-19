5f414c66855a2f5cec1b0f896f318c1937da3c71
Ben Hmida Aymen <118215572+AymenBH52@users.noreply.github.com>
2024-03-19 18:10:46 +0100

    Récupération des produits par catégories

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -8,14 +8,19 @@ import { ProductsService } from '../../services/products.service';
 })
 export class AllProductsComponent implements OnInit{
  
-  products: any[] = [];
+  products: any[] = []; //Array pour stoker les produits
+  categories: any[] = []; //Array pour stoker les catégories
 
   constructor(private service:ProductsService) { }
 
   ngOnInit(): void {
     this.getProducts(); //Appel de la méthode getProducts() pour afficher les produits lors de lancemnt de l'application
+    this.getCategories();
     
   }
+
+
+  //) Récupérer tous les produits
 
   getProducts() {
     //Prendre les données depuis le backend et stoker dans l'array products
@@ -25,12 +30,50 @@ export class AllProductsComponent implements OnInit{
       
       this.products = res
     }, error => {
-      console.log(error.message);
+      alert(error);
 
+    })
     }
-      )
-    }
+
       
+    //Récupérer toutes les catégories
 
+    getCategories() {
+      this.service.getAllCategories().subscribe((res:any) => {
+        // console.log(res);
+
+        this.categories = res
+
+      }, error => {
+        alert(error)
+      })
+    }
+
+
+    //Filtrer les produits par catégorie
+
+    filterCategory(event:any) {
+
+      let CategoryValue = event.target.value; //Récupérer la valeur de la catégorie sélectionnée
+
+      // console.log(CategoryValue); // Afficher la valeur de la catégorie sélectionnée
+
+      if(CategoryValue == "All") {
+        this.getProducts();
+      }
+
+      else{
+        this.getProductsCategory(CategoryValue); //Appel de la méthode getProductCategory() pour filtrer les produits par catégorie
+      }
+      
+    }
+
+    //Filtrer les produits par catégorie
+    getProductsCategory(keyword: string) {
+      this.service.getProductsByCategory(keyword).subscribe((res:any)=> {
+
+        this.products = res
+      })
+    }
     
 }
